db6694370bc317ddbdda5fd45365b6c82de0a06d
Lking540 <kingl2880@student.faytechcc.edu>
2024-06-20 13:44:13 -0400

    Add files via upload

---
+++ P1LAB1_LakeeshaKing.py
@@ -0,0 +1,15 @@
+ # Lakeesha King 
+ # 6/18/2024
+ # P1LAB1
+ # Write a code in IDLE. Display a message that welcomes a user for this course.
+
+first_name = input("Enter your first name:")
+last_name = input("Enter your last name:")
+
+
+print("Hello", first_name, last_name+"! Welcome to CTI-110")
+
+
+
+
+
