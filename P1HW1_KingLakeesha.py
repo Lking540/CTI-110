0dede6396123cdfad8849cf5871b9ddc051d65ea
Lking540 <kingl2880@student.faytechcc.edu>
2024-07-23 12:01:56 -0400

    Add files via upload

---
+++ P1HW1_KingLakeesha.py
@@ -0,0 +1,50 @@
+ # Lakesha King     
+ # 07/01/2024
+ # P1HW1
+ # Using Pyton's input and print functions
+
+
+Enter an integer as the base value:
+Enter an integer as the exponent:
+
+
+
+8 raised to the power of 4 is 4096 !!
+
+
+
+----- Addition and Subtraction-----
+
+
+
+
+Enter a starting integer: 11
+============================= RESTART: F:\P1HW1.py =============================
+----- Calculating Exponenets-----
+
+
+
+
+Enter an integer as the base value:
+Enter an integer as the exponent:
+
+
+
+8 raised to the power of 4 is 4096 !!
+
+
+
+----- Addition and Subtraction-----
+
+
+
+
+Enter a starting integer: 11
+Enter an integer to add: 7
+Enter an integer to subtract: 5
+
+
+
+11 + 7 - 5 is equal to 13
+
+
