f9031ee21b7462f2dcd6232fcb671636b422db32
Lking540 <kingl2880@student.faytechcc.edu>
2024-07-23 12:05:36 -0400

    Add files via upload

---
+++ P2lab2_KingLakeesha.py
@@ -0,0 +1,26 @@
+#Lakeesha King
+#6/27/2024
+#P2Lab2
+#program that creates a dictionary where the key and value pairs
+
+
+#create dictionary
+cars={"camaro":18.21, "Prius": 52.36, "Model S":110, "Silverado":26}
+#Dispay keys
+keys= cars.keys()     
+print(keys)
+print()
+#get intput from user
+car_input=input("Enter a vehicle to see it's mpg:")
+print()
+# Get the mpg associated with the vechicle
+mpg_output = cars[car_input]
+print(f'the{car_input} gets{mpg_output} mpg.\n')
+#Get the distance
+distance = float(input(f'How many miles will you drive the {car_input}?'))
+print()
+#Calculate gallons of gas needed
+gallons = distance/mpg_output
+# Print gallons needed to drive the car
+print(f'{gallons:.2f}gallons(s) of gas are needed to drive the car {car_input}{distance} miles.')
+
